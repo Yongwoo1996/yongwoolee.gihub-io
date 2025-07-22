c84250622b8316e39b93e68abd1d001f523e1e36
Yongwoo1996 <130029295+Yongwoo1996@users.noreply.github.com>
2025-07-22 13:52:21 +0900

    Add files via upload

---
+++ js/publicationsData.js
@@ -0,0 +1,62 @@
+const publications = [
+  {
+    imagePath: "img/thumbnails/panacea.png",
+    title: "Panacea: Novel DNN Accelerator using Accuracy-Preserving Asymmetric Quantization and Energy-Saving Bit-Slice Sparsity",
+    pdfLink: "https://arxiv.org/abs/2412.10059",
+    codeLinks: {
+      Slides: "../files/HPCA25_slides.pdf",
+    },
+    authors:
+      "<b>Dongyun Kam</b>, Myeongji Yun, Sunwoo Yoo, Seungwoo Hong, Zhengya Zhang, Youngjoo Lee",
+    conference: "IEEE International Symposium on High-Performance Computer Architecture (HPCA-31), 2025",
+    selected: true,
+  },
+  {
+    imagePath: "img/thumbnails/boss.png",
+    title: "A 21.9 ns 15.7 Gbps/mm² (128, 15) BOSS FEC Decoder for 5G/6G URLLC Applications",
+    pdfLink: "https://ieeexplore.ieee.org/abstract/document/10454363",
+    codeLinks: {
+      Slides: "../files/ISSCC24_slides.pdf",
+    },
+    authors:
+      "<b>Dongyun Kam</b>, Sangbu Yun, Jeongwon Choe, Zhengya Zhang, Namyoon Lee, Youngjoo Lee",
+    conference: "IEEE International Solid-State Circuits Conference (ISSCC), 2024",
+    selected: true,
+  },
+  {
+    imagePath: "img/thumbnails/overlap_polar.png",
+    title: "Low-latency SCL polar decoder architecture using overlapped pruning operations",
+    pdfLink: "https://ieeexplore.ieee.org/abstract/document/10014433",
+    codeLinks: {},
+    authors:
+      "<b>Dongyun Kam</b>, Byeong Yong Kong, Youngjoo Lee",
+    conference: "IEEE Transactions on Circuits and Systems I: Regular Papers (TCAS-I), 2023",
+    selected: true,
+  },
+  {
+    imagePath: "img/thumbnails/ternary.png",
+    title: "Design and Evaluation Frameworks for Advanced RISC-based Ternary Processor",
+    pdfLink: "https://ieeexplore.ieee.org/abstract/document/9774584",
+    codeLinks: {
+      Slides: "../files/DATE22_slides.pdf",
+    },
+    authors:
+      "<b>Dongyun Kam*</b>, Jung Gyu Min*, Jongho Yoon, Sunmean Kim, Seokhyeong Kang, Youngjoo Lee",
+    conference: "Design, Automation & Test in Europe Conference & Exhibition (DATE), 2022",
+    selected: true,
+  },
+ {
+    imagePath: "img/thumbnails/mem_polar.png",
+    title: "A 1.1μs 1.56Gb/s/mm2 Cost-Efficient Large-List SCL Polar Decoder Using Fully-Reusable LLR Buffers in 28nm CMOS Technology",
+    pdfLink: "https://ieeexplore.ieee.org/abstract/document/9830317",
+    codeLinks: {
+      Slides: "../files/VLSI22_slides.pdf",
+    },
+    authors:
+      "<b>Dongyun Kam</b>, Byeong Yong Kong, Youngjoo Lee",
+    conference: "IEEE Symposium on VLSI Technology and Circuits (VLSI Technology and Circuits), 2022",
+    selected: true,
+  },
+];
+
+export { publications };
---
+++ js/selectedPublications.js
@@ -0,0 +1,85 @@
+import { publications } from "./publicationsData.js";
+
+document.addEventListener("DOMContentLoaded", function () {
+  const iconMappings = {
+    Code: "/img/icons/github.png",
+    Talk: "/img/icons/youtube.png",
+    Blog: "/img/icons/blog.png",
+    Slides: "img/icons/presentation.png",
+  };
+
+  // Function to generate code links buttons with icons if applicable
+  function generateCodeLinksButtons(codeLinks) {
+    return Object.entries(codeLinks)
+      .map(([key, url]) => {
+        const iconHtml = iconMappings[key]
+          ? `<img src="${iconMappings[key]}" alt="${key} icon" style="height: 16px; vertical-align: middle; margin-right: 3px; margin-left: -3px; margin-top: -13px; padding: 0; margin-bottom: -10px">`
+          : "";
+
+        const buttonType =
+          key === "Code" ? "btn-outline-primary" : "btn-outline-success";
+        return `<a class="btn ${buttonType} btn-page-header" href="${url}" target="_blank" rel="noopener">${iconHtml}${key}</a>`;
+      })
+      .join("\n");
+  }
+
+  function addPublication(item) {
+    const container = document.getElementById(
+      "selected-publications-container"
+    );
+    const codeLinksButton = generateCodeLinksButtons(item.codeLinks);
+
+    const htmlContent = `
+            <div class="flex-row">
+                <div class="flex-item flex-item-stretch flex-column">
+                    <img class="image max-width-500" src="${item.imagePath}">
+                </div>
+                <div class="flex-item flex-item-stretch-3 flex-column">
+                    <p class="text-large" style="margin-bottom:0;">
+                        <a href="${item.pdfLink}"><b>${item.title}</b></a>
+                    </p>
+                    <p class="text" style="margin:0; padding-top:0; font-size:11pt">
+                        ${item.authors}<br>
+                    </p>
+                    <p class="text" style="margin:0; padding-top:0;">
+                        ${item.conference}
+                    </p>
+                    <div class="button-container">
+                        ${codeLinksButton}
+                    </div>
+
+                </div>
+            </div>
+            <div><p class="text"></p></div>
+            <div><p class="text"></p></div>
+        `;
+    container.innerHTML += htmlContent;
+  }
+
+  publications.filter((pub) => pub.selected).forEach(addPublication);
+
+  const container2 = document.getElementById("publications-container");
+  const fragment = document.createDocumentFragment();
+  publications.forEach((publication) => {
+    const div = document.createElement("div");
+    const codeLinksButton = generateCodeLinksButtons(publication.codeLinks);
+    const publicationHtml = `
+		<li>
+        <p class="text-large" style="margin-bottom:0;">
+            <a href="${publication.pdfLink}">
+                <b>${publication.title}</b></a>
+        </p>
+        <p class="text" style="margin:0; padding-top:0; font-size:11pt">
+            ${publication.authors}<br>
+        </p>
+        <p class="text" style="margin:0; padding-top:0;">
+            ${publication.conference}
+        </p>
+        ${codeLinksButton}
+		</li>
+    `;
+    div.innerHTML = publicationHtml;
+    fragment.appendChild(div);
+  });
+  container2.appendChild(fragment);
+});
